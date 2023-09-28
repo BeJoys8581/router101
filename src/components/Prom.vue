5f4b8fc4c10438d376589b7ec290cc2415647451
BeJoys8581 <beambeambenz81@gmail.com>
2023-09-28 20:06:42 +0700

    test5

--- src/components/Prom.vue
+++ src/components/Prom.vue
@@ -9,7 +9,7 @@ const TeaList = ref([
     { id: 1, name: 'Tranquil Infusions', img: "https://media-cdn.tripadvisor.com/media/photo-s/28/91/1e/26/our-store.jpg" },
     { id: 2, name: 'SereniTea Corner', img: "https://cdn.shopify.com/s/files/1/0323/4735/7321/files/anotherstoryshopify_480x480.jpg?v=1582107567" },
     { id: 3, name: 'Steeped Harmony', img: "https://www.londonperfect.com/cdn-cgi/image/width=1680,height=1058,fit=cover,quality=80,format=auto,onerror=redirect,metadata=none/https://www.londonperfect.com/blog/wp-content/uploads/2018/08/TWG-Tea-Knightsbridge-3.jpg" },
-    { id: 4, name: 'Mystic Brew Haven', img: "https://www.italianbark.com/wp-content/uploads/2015/03/Paperandtea_store_05-600x332.jpg" },
+    { id: 4, name: 'Mystic Brew Haven', img: "https://cdn.shopify.com/s/files/1/0323/4735/7321/files/onenimmanshopshopify_480x480.jpg?v=1582107013" },
     { id: 5, name: 'Chai Serendipity', img: "https://image.sistacafe.com/images/uploads/content_image/image/388931/1498817445-%E0%B8%A3%E0%B9%89%E0%B8%B2%E0%B8%99_%E0%B8%8A%E0%B8%B2_the_tea_house_%E0%B8%8A%E0%B9%88%E0%B8%B2%E0%B8%87%E0%B8%8A%E0%B8%B8%E0%B9%88%E0%B8%A2_%E0%B9%80%E0%B8%9B%E0%B8%B4%E0%B8%94%E0%B9%80%E0%B8%A1%E0%B8%B7%E0%B9%88%E0%B8%AD%E0%B9%84%E0%B8%AB%E0%B8%A3%E0%B9%88_%E0%B9%81%E0%B8%9C%E0%B8%99%E0%B8%97%E0%B8%B5%E0%B9%88%E0%B8%8A%E0%B9%88%E0%B8%B2%E0%B8%87%E0%B8%8A%E0%B8%B8%E0%B9%88%E0%B8%A23.jpg" },
     { id: 6, name: 'Chai Chat', img: "https://static.dezeen.com/uploads/2018/10/teatulia-russell-sage-interiors-retail-bars-london-uk_dezeen_2364_col_1.jpg" }
 ]);
