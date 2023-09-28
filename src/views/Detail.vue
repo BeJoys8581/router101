39fe10c90a383cb933881ea191f0cbff669f4101
BeJoys8581 <beambeambenz81@gmail.com>
2023-09-28 18:24:18 +0700

    test 2

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -6,7 +6,7 @@ import{ ref } from "vue";
     { id: 2, name: 'SereniTea Corner', img: "https://cdn.shopify.com/s/files/1/0323/4735/7321/files/anotherstoryshopify_480x480.jpg?v=1582107567" },
     { id: 3, name: 'Steeped Harmony', img: "https://www.londonperfect.com/cdn-cgi/image/width=1680,height=1058,fit=cover,quality=80,format=auto,onerror=redirect,metadata=none/https://www.londonperfect.com/blog/wp-content/uploads/2018/08/TWG-Tea-Knightsbridge-3.jpg" },
     { id: 4, name: 'Mystic Brew Haven', img: "https://cdn.shopify.com/s/files/1/0323/4735/7321/files/onenimmanshopshopify_480x480.jpg?v=1582107013" },
-    { id: 5, name: 'Chai Serendipity', img: "https://images.adsttc.com/media/images/5d31/fd66/284d/d1df/cf00/0142/large_jpg/Spiritea-New_Practice_Studio-7.jpg?1563557172" },
+    { id: 5, name: 'Chai Serendipity', img: "https://image.sistacafe.com/images/uploads/content_image/image/388931/1498817445-%E0%B8%A3%E0%B9%89%E0%B8%B2%E0%B8%99_%E0%B8%8A%E0%B8%B2_the_tea_house_%E0%B8%8A%E0%B9%88%E0%B8%B2%E0%B8%87%E0%B8%8A%E0%B8%B8%E0%B9%88%E0%B8%A2_%E0%B9%80%E0%B8%9B%E0%B8%B4%E0%B8%94%E0%B9%80%E0%B8%A1%E0%B8%B7%E0%B9%88%E0%B8%AD%E0%B9%84%E0%B8%AB%E0%B8%A3%E0%B9%88_%E0%B9%81%E0%B8%9C%E0%B8%99%E0%B8%97%E0%B8%B5%E0%B9%88%E0%B8%8A%E0%B9%88%E0%B8%B2%E0%B8%87%E0%B8%8A%E0%B8%B8%E0%B9%88%E0%B8%A23.jpg" },
     { id: 6, name: 'Chai Chat', img: "https://static.dezeen.com/uploads/2018/10/teatulia-russell-sage-interiors-retail-bars-london-uk_dezeen_2364_col_1.jpg" }
   ]);
 
@@ -29,7 +29,7 @@ import{ ref } from "vue";
       >
         <img :src="Tea.img" width="450" height="300" style="margin-top: 10px;" />
       </div>
-      <div class="d-flex gap-2 justify-content-center ">
+      <div class="d-flex gap-1 justify-content-center ">
         <RouterLink :to="{name:'Tea', params: {id: Tea.id }}" class="button" type="button">
           Tea Details {{ index+1 }} 
         </RouterLink>
@@ -44,9 +44,9 @@ import{ ref } from "vue";
 <style>
 
 img{
-  width: 300px;
-  height: 300px;
-
+  width: 100%;
+  height: auto;
+  border-radius: 10%;
 }
 
 .fuck{
@@ -54,7 +54,7 @@ img{
   grid-template-columns: 1fr 1fr 1fr ;
 }
 .button {
- --color: #560bad;
+ --color: #016c15;
  font-family: inherit;
  display: inline-block;
  width: 8em;
@@ -153,5 +153,6 @@ img{
     flex: 1;
   }
 }
+
 
 </style>
